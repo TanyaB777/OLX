d9a7035dbfa7f5cef503e9ec8b92a8603107b754
Anton_Bilyi <Anton_Bilyi@epam.com>
2024-06-09 22:30:39 +0300

    First branch creation and push

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello, my name is Java app for ALevel students!");
     }
 }
