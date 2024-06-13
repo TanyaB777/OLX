cb5893a22f8920d45d0e4809970de3b4237f08c6
tanya_bilka <tanyawbw7@gmail.com>
2024-06-13 16:24:29 +0300

    HW 1

---
+++ src/main/java/hw_1/Task1.java
@@ -0,0 +1,13 @@
+package hw_1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String name = "Tanya";
+        int age = 17;
+        float weight = 55.5f;
+
+        System.out.println("--- Home Work 1 ---");
+        System.out.printf("Ім'я=%s, Вік=%d, Вага=%.2f", name, age, weight);
+        System.out.println();
+    }
+}
