bcc027b0197481aa3d5d6ec5ba11e2f3bfb54010
tanya_bilka <tanyawbw7@gmail.com>
2024-07-08 14:36:22 +0300

    Changes according the comments

---
+++ src/main/java/hw_7/Square.java
@@ -0,0 +1,25 @@
+package hw_7;
+
+class Square extends Figure {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4 * side;
+    }
+
+    @Override
+    public String toString() {
+        return "Square [side=" + side + ", area=" + getArea() + ", perimeter=" + getPerimeter() + "]";
+    }
+}
+
