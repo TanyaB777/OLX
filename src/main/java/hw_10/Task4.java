6138433e71b5cc44cc1d21b3642f67d9ff5197ef
tanya_bilka <tanyawbw7@gmail.com>
2024-07-17 18:56:02 +0300

    Changes in Task 4 was made

--- src/main/java/hw_10/Task4.java
+++ src/main/java/hw_10/Task4.java
@@ -4,16 +4,16 @@ import java.util.HashSet;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.Arrays;
 
 public class Task4 {
     public static void main(String[] args) {
 
         int[] numbers = {1, 2, 6, 9, 3, 5, 1, 6, 8, 6, 4, 4, 4};
-
         Set<Integer> set = new HashSet<>();
 
-        for (int number : numbers) {
-            set.add(number);
+        for (int num : numbers) {
+            set.add(num);
         }
 
         System.out.println("Set elements: " + set);
