143e40d5fd29071aad99bf7ff14f96fd8455989d
tanya_bilka <tanyawbw7@gmail.com>
2024-07-18 18:23:23 +0300

    Changes according the comments 1

--- src/main/java/hw_10/Person.java
+++ src/main/java/hw_10/Person.java
@@ -14,16 +14,10 @@ public class Person {
         return age;
     }
 
-
     Person(String name, String surname, int age, double salary) {
         this.name = name;
         this.surname = surname;
         this.age = age;
         this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{name='" + name + "', surname='" + surname + "', age=" + age + ", salary=" + salary + "}";
     }
 }
